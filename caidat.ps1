41acb9c3208285884ccb9d91070a1780f7c58f42
dtrbinh <68652319+dtrbinh@users.noreply.github.com>
2021-04-22 18:52:27 +0700

    Add files via upload

---
+++ caidat.ps1
@@ -0,0 +1,3 @@
+Frg-ArgSverjnyyCebsvyr -Cebsvyr Qbznva,Choyvp,Cevingr -Ranoyrq Snyfr
+& {$C = $rai:GRZC + '\puebzrerzbgrqrfxgbcubfg.zfv'; Vaibxr-JroErdhrfg 'uggcf://qy.tbbtyr.pbz/rqtrqy/puebzr-erzbgr-qrfxgbc/puebzrerzbgrqrfxgbcubfg.zfv' -BhgSvyr $C; Fgneg-Cebprff $C -Jnvg; Erzbir-Vgrz $C}
+& {$C = $rai:GRZC + '\puebzr_vafgnyyre.rkr'; Vaibxr-JroErdhrfg 'uggcf://qy.tbbtyr.pbz/puebzr/vafgnyy/yngrfg/puebzr_vafgnyyre.rkr' -BhgSvyr $C; Fgneg-Cebprff -SvyrCngu $C -Netf '/vafgnyy' -Ireo EhaNf -Jnvg; Erzbir-Vgrz $C}
---
+++ time.ps1
@@ -0,0 +1,6 @@
+$v = 360
+qb {
+Jevgr-Ubfg $v
+Fyrrc 60
+$v--
+} juvyr ($v -tg 0)
